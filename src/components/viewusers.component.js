1ba5519a82e541e372389485543497d6c6f5ac39
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-06-07 21:51:33 +0530

    Components of view users list added

---
+++ src/components/viewusers.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+
+export default class ViewUsers extends Component{
+    render(){
+        return(
+            <div className='ViewUsers'>
+                <p>This is a component of viewusers</p>
+            </div>
+
+        );
+    }
+}
