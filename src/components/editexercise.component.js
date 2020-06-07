aa28c759922b4fe9fe71a7b73fed2eb65db9b5e8
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-06-07 21:48:45 +0530

    Components of edit exercise added

---
+++ src/components/editexercise.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+
+export default class EditExercise extends Component{
+    render(){
+        return(
+            <div className='EditExercise'>
+                <p>This is a component of Edit Exercise</p>
+            </div>
+
+        );
+    }
+}
