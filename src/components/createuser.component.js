63896ec75f2e5b06f79ca878d75414037b7d25d2
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-06-07 21:49:20 +0530

    Components of create user added

---
+++ src/components/createuser.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+
+export default class CreateUser extends Component{
+    render(){
+        return(
+            <div className='CreateUser'>
+                <p>This is a component of Create User</p>
+            </div>
+
+        );
+    }
+}
