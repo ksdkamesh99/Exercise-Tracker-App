d70fd327542a1cba48b33530a4d82c34545de738
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-06-07 21:50:20 +0530

    Components of delete exercise added

---
+++ src/components/deleteexercise.component.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+
+export default class DeleteExercise extends Component{
+    render(){
+        return(
+            <div className='DeleteExercise'>
+                <p>This is a component of Delete Exercise</p>
+            </div>
+
+        );
+    }
+}
