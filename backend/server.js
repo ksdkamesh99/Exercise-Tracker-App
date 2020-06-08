9d42657fb7405592228e749978c6ed880f2848a1
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-06-08 21:44:32 +0530

    Server File added

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const cors=require('cors');
+const mongoose=require('mongoose');
+
+
+require('dotenv').config();
+
+const app =express()
+const port= process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =process.env.ATLAS_URI;
+mongoose.connect(uri,{useNewUrlParser: true ,useCreateIndex: true,useUnifiedTopology: true});
+
+const connection=mongoose.connection;
+
+connection.once('open' ,()=>{
+    console.log("mongoose database established");
+});
+
+const ExerciseRouter=require('./routes/exercises');
+const UserRouter=require('./routes/users');
+
+app.use('/exercises',ExerciseRouter);
+app.use('/users',UserRouter);
+
+app.listen(port,()=>{
+    console.log("Server Started at port: ",port);
+});
